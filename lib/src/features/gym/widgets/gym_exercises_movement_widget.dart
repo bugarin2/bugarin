5227728a81754966a53f8a723a380658c45ccaa5
“Ivan <ivanfernanda97@gmail.com>
2024-03-24 09:38:51 +0700

    [refactor] FlutterPng: Images for assets

--- lib/src/features/gym/widgets/gym_exercises_movement_widget.dart
+++ lib/src/features/gym/widgets/gym_exercises_movement_widget.dart
@@ -5,6 +5,32 @@ class GymExercisesMovementWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('Abs');
+    return Container(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              leading: ImageIcon(AssetImage("images/flutter.png")),
+              title: Text(
+                'Abs',
+                style: TextStyle(fontSize: 20),
+              ),
+              subtitle: Text('28 Exercises'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('Next'),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 20),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
